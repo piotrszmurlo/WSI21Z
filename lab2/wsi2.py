95909f74239499d2db4c13c64a85d2e569e06062
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-10-26 00:20:46 +0200

    postep w zadaniu 2

--- lab2/wsi2.py
+++ lab2/wsi2.py
@@ -1,45 +1,37 @@
-from random import gauss, randint, uniform
+from random import gauss, randint, random
+import numpy as np
 
 def find_minimum(iterations, population_size, tournament_size, elite_size, sigma, mutation_p):
-    population = []
+    f = lambda x: x**2
     upp_bound = 10
     low_bound = -10
-    for i in range(0, population_size):
-        r = uniform(low_bound, upp_bound)
-        population.append((r, r**2)) 
+    population = np.random.uniform(low_bound, upp_bound, population_size)
+    best_point = min(population, key=lambda x: x**2)
     i = 0
     while i < iterations:
-        population = sorted(population, key=lambda x: x[1])
+        temp_population = tournament_select(population, tournament_size)
+        for point in temp_population:
+            if random() < mutation_p:
+                point += gauss(0, 1)*sigma
+        new_best_point = min(population, key=lambda x: x**2)
+        if f(new_best_point) < f(best_point):
+            best_point = new_best_point
 
-        rep = []
-        for j in range(0, population_size):
-            winner = randint(0, population_size - 1)
-            for k in range(0, tournament_size - 1):
-                new_index = randint(0, population_size - 1)
-                if new_index < winner:
-                    winner = new_index
-            rep.append(population[winner])
-        for r in rep:
-            r[0] += gauss(0,1)*sigma
-            r[1] = r[0]**2
+def tournament_select(population, tournament_size=2) -> np.ndarray:
+    sorted_population = np.array(sorted(population, key=lambda x: x**2))
+    new_population = np.zeros_like(population)
+    population_size = population.size
+    for i in range(population_size):
+        winner_index = randint(0, population_size - 1)
+        for _123 in range(tournament_size - 1):
+            new_index = randint(0, population_size - 1)
+            if new_index < winner_index:
+                winner_index = new_index
+        new_population[i] = sorted_population[winner_index]
+    return new_population
 
 
 
 if __name__ == "__main__":
-    population = []
     population_size = 10
-    for i in range(0, population_size):
-        r = uniform(-2, 2)
-        population.append((r, r**2))
-    population = sorted(population, key=lambda x: x[1])
-    rep = []
-    for j in range(0, population_size):
-        winner = randint(0, population_size - 1)
-        for k in range(0, 1):
-            new_index = randint(0, population_size - 1)
-            if new_index < winner:
-                winner = new_index
-        rep.append(population[winner])
-    print(rep)
-    # for r in rep:
-    #     r
+    population = np.random.uniform(-5, 5, population_size)
