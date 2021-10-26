cc49fd990df82bb352b319fa1ca59a64a1340d53
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-10-26 13:34:16 +0200

    dziala dla x^2

--- lab2/wsi2.py
+++ lab2/wsi2.py
@@ -1,37 +1,73 @@
 from random import gauss, randint, random
 import numpy as np
 
-def find_minimum(iterations, population_size, tournament_size, elite_size, sigma, mutation_p):
+
+def find_minimum(iterations, population_size, tournament_size=2, elite_size=1, alfa=0.1, sigma=0.1, mutation_p=0.5, cross_p=0.5):
     f = lambda x: x**2
     upp_bound = 10
     low_bound = -10
     population = np.random.uniform(low_bound, upp_bound, population_size)
     best_point = min(population, key=lambda x: x**2)
     i = 0
+    R_population = np.array([])
     while i < iterations:
-        temp_population = tournament_select(population, tournament_size)
-        for point in temp_population:
-            if random() < mutation_p:
-                point += gauss(0, 1)*sigma
-        new_best_point = min(population, key=lambda x: x**2)
+        R_population = tournament_select(population, tournament_size)
+        M_population = cross_and_mutate(R_population, mutation_p, cross_p, alfa, sigma)
+        new_best_point = min(M_population, key=lambda x: x**2)
         if f(new_best_point) < f(best_point):
             best_point = new_best_point
+        population = np.unique(np.concatenate((population[:elite_size], M_population)))[:-elite_size]
+        i += 1
+    return best_point
+
 
 def tournament_select(population, tournament_size=2) -> np.ndarray:
     sorted_population = np.array(sorted(population, key=lambda x: x**2))
     new_population = np.zeros_like(population)
-    population_size = population.size
-    for i in range(population_size):
-        winner_index = randint(0, population_size - 1)
+    for i in range(population.size):
+        winner_index = randint(0, population.size - 1)
         for _123 in range(tournament_size - 1):
-            new_index = randint(0, population_size - 1)
+            new_index = randint(0, population.size - 1)
             if new_index < winner_index:
                 winner_index = new_index
         new_population[i] = sorted_population[winner_index]
     return new_population
 
 
+def cross_and_mutate(population, mutation_p, cross_p, alfa, sigma) -> np.ndarray:
+    population_size = len(population)
+    M_population = np.empty_like(population)
+    indexes = np.array((range(M_population.size)))
+    i = 0
+    while i < population_size:
+        parents = np.random.choice(indexes, size=2, replace=False)
+        if random() < cross_p:
+            M_population[i] = alfa * population[parents[0]] + (1 - alfa) * population[parents[1]]
+            if i + 1 < population.size:
+                M_population[i + 1] = alfa * population[parents[1]] + (1 - alfa) * population[parents[0]]
+        else:
+            M_population[i] = population[parents[0]]
+            if i + 1 < population.size:
+                M_population[i + 1] = population[parents[1]]
+        i += 2
+    for j in range(M_population.size):
+        if random() < mutation_p:
+            M_population[j] += gauss(0, 1)*sigma
+    return M_population
+
+
+def main():
+    iterations = 500
+    population_size = 20
+    tournament_size=2
+    elite_size=1
+    alfa=0.1
+    sigma=0.1
+    mutation_p=1
+    cross_p=0
+    population = np.random.uniform(-10, 10, population_size)
+    print(find_minimum(iterations, population_size, tournament_size, elite_size, alfa, sigma, mutation_p, cross_p))
+
 
 if __name__ == "__main__":
-    population_size = 10
-    population = np.random.uniform(-5, 5, population_size)
+    main()
