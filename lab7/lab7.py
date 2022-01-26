a5d93cd1975996a5179597f5bf91170b905360ff
piotrszmurlo <piotr.szmurlo7@gmail.com>
2022-01-26 16:26:19 +0100

    data generation done

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -3,15 +3,13 @@ from pgmpy.factors.discrete import TabularCPD
 import csv
 import numpy as np
 import sys
+import os
+
 N = 10
-# uncond1_input = {'name' : 'studies', 'p' : 0.64}
-# uncond2_input = {'name' : 'loneliness', 'p' : 0.15}
-# cond1_input = {}
-# cond2_input = {}
 
 p_input = {
-    'studies' : [[0.64], [0.36]],
-    'loneliness' : [[0.15], [0.85]],
+    'studies' : [[0.36], [0.64]],
+    'loneliness' : [[0.85], [0.15]],
     'overeating' : [[0.85, 0.35, 0.7, 0.15],
                     [0.15, 0.65, 0.3, 0.85]],
     'depression' : [[0.8, 0.3], [0.2, 0.7]]
@@ -25,38 +23,37 @@ edges_input = {
 
 edges = [(edge, edges_input[edge]) for edge in edges_input]
 unconditional_nodes = [node for node in edges_input if node not in edges_input.values()]
-print(unconditional_nodes)
-studies = TabularCPD(variable='studies', variable_card=2, values=[[0.64], [0.36]])
-unconditional_cpd = []
+cpds = []
 for node in unconditional_nodes:
-    unconditional_cpd.append(TabularCPD(variable=node, variable_card=2, values=p_input[node]))
-# loneliness = TabularCPD(variable='loneliness', variable_card=2, values=[[0.15], [0.85]])
+    cpds.append(TabularCPD(variable=node, variable_card=2, values=p_input[node]))
+for node in p_input:
+    if node not in unconditional_nodes:
+        evidence = [edge[0] for edge in edges if edge[1] == node]
+        cpds.append(TabularCPD(variable=node, variable_card=2, values=p_input[node], evidence=evidence, evidence_card=[2]*len(evidence)))
 
-# overeating = TabularCPD(variable='overeating', variable_card=2,
-#                  values=[[0.85, 0.35, 0.7, 0.15],
-#                          [0.15, 0.65, 0.3, 0.85]],
-#                  evidence=['studies', 'loneliness'],
-#                  evidence_card=[2, 2])
+DAG = bn.make_DAG(edges)
+DAG = bn.make_DAG(DAG, CPD=cpds)
 
-# depression = TabularCPD(variable='depression', variable_card=2, values=[[0.8, 0.3], [0.2, 0.7]], evidence=['overeating'], evidence_card=[2])
-
-
-
-# DAG = bn.make_DAG(edges)
-# DAG = bn.make_DAG(DAG, CPD=[studies, loneliness, overeating, depression])
-
-# bn.print_CPD(DAG)
+bn.print_CPD(DAG)
 
 
-# with open('out.csv', 'w', newline = '' ) as outFile:
-#     writer = csv.writer(outFile)
-#     writer.writerow([uncond1_input['name'], uncond2_input['name'], 'overeating', 'depression'])
-#     for n in range(N):
-#         uncond1_rand = np.random.binomial(1, uncond1_input['p'])
-#         uncond2_rand = np.random.binomial(1, uncond2_input['p'])
-#         cond1_p = bn.inference.fit(DAG, variables=['overeating'], evidence={'studies': uncond1_rand, 'loneliness' : uncond2_rand})
-#         cond1_rand = np.random.binomial(1, cond1_p.df['p'][1])
-#         cond2_p = bn.inference.fit(DAG, variables=['depression'], evidence={'overeating': cond1_rand})
-#         cond2_rand = np.random.binomial(1, cond2_p.df['p'][1])
+with open('out.csv', 'w', newline = '') as outFile:
+    writer = csv.DictWriter(outFile, fieldnames=p_input.keys())
+    writer.writeheader()
+    real_stdout = sys.stdout
+    sys.stdout = open(os.devnull, "w")
+    for n in range(1000):
+        row = {}
+        for node in unconditional_nodes:
+            row[node] = np.random.binomial(1, p_input[node][1][0])
+        for node in p_input:
+            if node not in unconditional_nodes:
+                evidence = {}
+                for edge in edges:
+                    if edge[1] == node:
+                        evidence[edge[0]] = row[edge[0]]
+                cond_p = bn.inference.fit(DAG, variables=[node], evidence=evidence).df['p'][1]
+                row[node] = np.random.binomial(1, cond_p)
+        writer.writerow(row)
+    sys.stdout = real_stdout
 
-#         writer.writerow([uncond1_rand, uncond2_rand, cond1_rand, cond2_rand])
--- lab7/lab7got.py
+++ lab7/lab7got.py
@@ -1,23 +1,29 @@
 import bnlearn as bn
 from pgmpy.factors.discrete import TabularCPD
+edges_input = {
+    'studies' : 'overeating',
+    'loneliness' : 'overeating',
+    'overeating' : 'depression'
+}
 
-rain = TabularCPD(variable='rain', variable_card=2, values=[[0.7], [0.3]])
-sprinkler = TabularCPD(variable='sprinkler', variable_card=2,
-                       values=[[0.2, 0.9],
-                               [0.8, 0.1]],
-                       evidence=['rain'], evidence_card=[2])
-wet = TabularCPD(variable='wet', variable_card=2,
-                 values=[[0.99, 0.1, 0.3, 0.05],
-                         [0.01, 0.9, 0.7, 0.95]],
-                 evidence=['rain', 'sprinkler'],
-                 evidence_card=[2, 2])
+edges = [(edge, edges_input[edge]) for edge in edges_input]
+# rain = TabularCPD(variable='rain', variable_card=2, values=[[0.7], [0.3]])
+# sprinkler = TabularCPD(variable='sprinkler', variable_card=2,
+#                        values=[[0.2, 0.9],
+#                                [0.8, 0.1]],
+#                        evidence=['rain'], evidence_card=[2])
+# wet = TabularCPD(variable='wet', variable_card=2,
+#                  values=[[0.99, 0.1, 0.3, 0.05],
+#                          [0.01, 0.9, 0.7, 0.95]],
+#                  evidence=['rain', 'sprinkler'],
+#                  evidence_card=[2, 2])
 
-edges = [('rain', 'sprinkler'),
-         ('rain', 'wet'),
-         ('sprinkler', 'wet')]
+# edges = [('rain', 'sprinkler'),
+#          ('rain', 'wet'),
+#          ('sprinkler', 'wet')]
 
-DAG = bn.make_DAG(edges)
-DAG = bn.make_DAG(DAG, CPD=[rain, sprinkler, wet])
+# DAG = bn.make_DAG(edges)
+# DAG = bn.make_DAG(DAG, CPD=[rain, sprinkler, wet])
 
 # bn.print_CPD(DAG)
 
@@ -29,5 +35,22 @@ DAG = bn.make_DAG(DAG, CPD=[rain, sprinkler, wet])
 # # Probability of rain when wet
 # bn.inference.fit(DAG, variables=['rain'], evidence={'wet': 1})
 # # Probability of rain and/or sprinkler when wet
-table = bn.inference.fit(DAG, variables=['rain', 'sprinkler'], evidence={'wet': 1})
-print(table.df['p'])
+# table = bn.inference.fit(DAG, variables=['rain', 'sprinkler'], evidence={'wet': 1})
+# print(table.df['p'])
+
+loneliness = TabularCPD(variable='loneliness', variable_card=2, values=[[0.15], [0.85]])
+studies = TabularCPD(variable='studies', variable_card=2, values=[[0.64], [0.36]])
+
+overeating = TabularCPD(variable='overeating', variable_card=2,
+                 values=[[0.85, 0.35, 0.7, 0.15],
+                         [0.15, 0.65, 0.3, 0.85]],
+                 evidence=['studies', 'loneliness'],
+                 evidence_card=[2, 2])
+
+depression = TabularCPD(variable='depression', variable_card=2, values=[[0.8, 0.3], [0.2, 0.7]], evidence=['overeating'], evidence_card=[2])
+
+
+DAG = bn.make_DAG(edges)
+DAG = bn.make_DAG(DAG, CPD=[loneliness, studies, overeating, depression])
+
+bn.print_CPD(DAG)
